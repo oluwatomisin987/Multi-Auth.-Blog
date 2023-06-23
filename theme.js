782b3b5c0e5aee011535831757ce8e4dc3a25795
OLUWATOMISIN <hardex.tommy@gmail.com>
2023-06-23 13:58:43 +0100

    23-06

--- theme.js
+++ theme.js
@@ -1,37 +1,22 @@
-var content = document.getElementsByTagName('body')[0];
-// var content1 = document.getElementsByClassName(nav-div-2)[1];
-        var darkMode = document.getElementById('dark-change');
-        darkMode.addEventListener('click', function(){
-            darkMode.classList.toggle('active');
-            content.classList.toggle('night');
-        })
+var content = document.getElementsByTagName("body")[0];
+
+var darkMode = document.getElementById("dark-change");
+
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("active");
+  content.classList.toggle("night");
+  // darkMode.classList.toggle("active");
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /* Set the width of the sidebar to 250px (show it) */
+/* Set the width of the sidebar to 250px (show it) */
 function openNav() {
   document.getElementById("mySidepanel").style.width = "230px";
-
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
-
 }
-
