27c644da07ade2f60896b03971a0a8817696722c
OLUWATOMISIN <hardex.tommy@gmail.com>
2023-07-07 18:51:19 +0100

    07-07

--- theme.js
+++ theme.js
@@ -1,12 +1,80 @@
-var content = document.querySelector(".bboddyy");
+// var content = document.querySelector(".bboddyy");
 
-var darkMode = document.getElementById("dark-change");
+// var darkMode = document.getElementById("dark-change");
 
-darkMode.addEventListener("click", function () {
-  darkMode.classList.toggle("active");
-  content.classList.toggle("night");
-  // darkMode.classList.toggle("active");
-});
+// darkMode.addEventListener("click", function () {
+//   darkMode.classList.toggle("active");
+//   content.classList.toggle("night");
+//   // darkMode.classList.toggle("active");
+// });
+
+
+
+
+
+
+
+
+/**
+ * This function sets the color mode of the app based
+ * on a variable stored in localStorage
+ */
+const setColorMode = () => {
+  const colorMode = localStorage.getItem("mode");
+  const href =
+    colorMode === "dark" ? "dark-mode.css" : "light-mode.css";
+  const link = document.getElementById("stylesheet");
+  link.setAttribute("href", href);
+};
+
+
+/**
+ * This function toggles the color mode and refreshes
+ * the page so the color mode can take effect
+ */
+const toggleColorMode = () => {
+  const colorMode = localStorage.getItem("mode");
+  const mode = colorMode === "dark" ? "light" : "dark";
+  localStorage.setItem("mode", mode);
+  location.reload();
+};
+
+
+window.onload = () => {
+  setColorMode(); // Set the color mode on load
+
+  const toggleBtn = document.getElementById("dark-change");
+  if (toggleBtn) {
+    // The toggle button is not on all pages
+    // this checks if the button is present before adding the event listener
+    toggleBtn.addEventListener("click", toggleColorMode);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
